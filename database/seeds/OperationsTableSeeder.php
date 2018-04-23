925a6c5294591f658bb09b75a9b34800766ce37f
Serhii Radkevych <radserhii@gmail.com>
2018-04-23 17:14:05 +0300

    Compute sum_usd for operations seeder

--- database/seeds/OperationsTableSeeder.php
+++ database/seeds/OperationsTableSeeder.php
@@ -16,7 +16,7 @@ class OperationsTableSeeder extends Seeder
         foreach (range(1,10) as $index) {
 
             $sum = $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 10000);
-            $sumUsd = $sum;
+            $sumUsd = $sum / App\Libs\ApiPrivatbank::getCourse('usd');
 
             DB::table('operations')->insert([
                 'title' => $faker->sentence($nbWords = 4, $variableNbWords = true),
